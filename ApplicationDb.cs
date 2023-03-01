dcde42807351b126f44d47f690472f2bf9450274
Marco Martinez <TheMyak@hotmail.com>
2023-02-28 23:10:51 -0600

    1.11

--- ApplicationDb.cs
+++ ApplicationDb.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Estacionamiento_WebApi.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace Estacionamiento_WebApi
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
 
+        public DbSet<Estacionamiento> Estacionamiento { get; set; }
     }
 }
